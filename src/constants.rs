61f6365256d15ca8c1bcef2682c951faa271c52f
jordan-m-finn <jordan.m.finn17@gmail.com>
2026-01-25 23:40:21 -0800

    Make constants public

--- src/constants.rs
+++ src/constants.rs
@@ -1,10 +1,10 @@
 // size of the physical memory ~words (ints)
-const PM_SIZE: usize = 524_288;
-const FRAME_SIZE: usize = 512;
-const NUM_FRAMES: usize = 1024;
-const DISK_BLOCKS: usize = 1024;
+pub const PM_SIZE: usize = 524_288;
+pub const FRAME_SIZE: usize = 512;
+pub const NUM_FRAMES: usize = 1024;
+pub const DISK_BLOCKS: usize = 1024;
 
 // bitmasks (u32 since bitwise operations 32-bit virtual addys are needed and mixing usize w/ u32
 // causes errors
-const MASK_9_BITS: u32 = 0x1FF;
-const MASK_18_BITS: u32 = 0x3FFFF;
+pub const MASK_9_BITS: u32 = 0x1FF;
+pub const MASK_18_BITS: u32 = 0x3FFFF;
